7edc50a5625cae21cfd51d7c659be4b10fc27a3e
fisonsvh <Fison23@icloud.com>
2020-06-15 11:06:05 +0300

    11/04

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/* var lefBorderWidth = 1;
+console.log (lefBorderWidth)
+const pi = 3.14;
+{
+    let second = 2;
+}
+console.log (second)
+
+ */
+
+let something;
+console.log(4/0)
+console.log(something)
+
+let person = {
+    name: "Jonh",
+    age: 25,
+    isMaried: false
+};
+console.log(person.isMaried)
+
+
+let arr = ['plum.png', 'orange.jpeg', 'apple.bmp'];
+console.log (arr[0])
